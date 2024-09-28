4fb950ed664e8c1120a963f101fd36d4eb558c4b
Ashik <ashikurashik20999@gmail.com>
2024-09-29 00:43:15 +0600

    second task added

---
+++ task_2.js
@@ -0,0 +1,18 @@
+const getEvenLengthNames = (array) =>
+{
+    const evenLengthNames = [];
+
+    for(let i = 0; i<array.length ; i++){
+        if(array[i].length % 2 === 0){
+            evenLengthNames.push(array[i]);
+        }
+    }
+    return evenLengthNames;
+}
+
+const array = ["Tanmoy", "Shawon", "Ariful", "Shiblee", "Farhan", "Eva"];
+
+
+console.log(getEvenLengthNames(array));
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+// // const getEvenLengthNames = (array) =>
+// // {
+// //     const evenLengthNames = [];
+
+// //     for(let i = 0; i<array.length ; i++){
+// //         if(array.length[i] % 2 === 0){
+// //             evenLengthNames.push(array[i]);
+// //         }
+// //     }
+// //     return evenLengthNames;
+// // }
+
+// // const array = ["Tanmoy", "Shawon", "Ariful", "Shiblee", "Farhan", "Eva"];
+
+
+// console.log(getEvenLengthNames(array));
