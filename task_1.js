de7d0388334e2fef67200bf76c3bfb9517d9b103
Ashik <ashikurashik20999@gmail.com>
2024-09-28 23:58:12 +0600

    Practice Problem 1 added

--- task_1.js
+++ task_1.js
@@ -1,3 +1,17 @@
 const multiply = (n1, n2, n3) => (n1*n2*n3)
 const result = multiply(1,2,3);
 console.log(result);
+
+const threeLinePrint = `
+I am a web Developer.
+I love to code.
+I love to eat biryani.
+`
+
+console.log(threeLinePrint);
+
+
+const twoParameter = (one , two=8) => (one + two)
+
+const resultForTP = twoParameter(8);
+console.log(resultForTP);
